cb76fd36da476bbe3c7a418663858e0c078f783f
2894638479 <2894638479@qq.com>
2025-05-27 23:12:46 +0800

    删除无用模块

--- src/main/java/name/fireballboom/FireballBoom.java
+++ src/main/java/name/fireballboom/FireballBoom.java
@@ -1,6 +1,5 @@
 package name.fireballboom;
 
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.LargeFireball;
@@ -8,16 +7,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class FireballBoom implements ModInitializer {
-	public static final String MOD_ID = "fireball-boom";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
-	}
+public class FireballBoom {
 	public static void summonFireball(Player player, InteractionHand interactionHand){
 		ItemStack stack = player.getItemInHand(interactionHand);
 		if(stack.is(Items.FIRE_CHARGE) && stack.getCount() > 0){
